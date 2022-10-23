126ee0663330f381d3fb2332aca9c5fae08ed1f4
alex mesa <alexmesa1990@hotmail.com>
2022-10-23 18:52:59 -0500

    Enrutado basico para productos

--- back/app.js
+++ back/app.js
@@ -1,4 +1,12 @@
 const express=require("express");
 const app = express();
 
+app.use(express.json());
+
+//importar rutas
+const productos=require("./routes/products")
+
+app.use('/api',productos) //Sujeto a decision (rutas del navegador)
+
 module.exports=app
+// enrutado basico para productos

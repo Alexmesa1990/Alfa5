9f63c039960138f40a62675243210e62ab192e59
alex mesa <alexmesa1990@hotmail.com>
2022-10-24 18:13:33 -0500

    haciendo pruebas agregando producto

--- back/models/productos.js
+++ back/models/productos.js
@@ -2,20 +2,20 @@ const mongoose=require("mongoose")
 
 const productosSchema=mongoose.Schema({
     nombre:{
-        type: String,
-        required: [true,"Por favor registra el nombre del producto."],
-        trim: true,
-        maxLenght: [120,"El nombre del producto no debe exceder los 120 caracteres."]
+        type:String,
+        required:[true,"Por favor registra el nombre del producto."],
+        trim:true,
+        maxLength:[120,"El nombre del producto no debe exceder los 120 caracteres."]
     },
     precio:{
         type: Number,
-        required: [true,"Por favor registra el precio del producto."],
-        maxLenght: [8,"El precio del producto no puede estar por encima de 99'999.999."],
+        required:[true,"Por favor registre el precio del producto."],
+        maxLength:[8, "El precio del producto no puede estar por encima de 99'999.999"],
         default: 0.0
     },
     descripcion:{
-        type: String,
-        required: [true,"Por favor registre una descripción para el producto."],
+      type:String,
+      required:[true,"Por favor registre una descripcion para el producto."]
     },
     calificacion:{
         type: Number,
@@ -23,69 +23,61 @@ const productosSchema=mongoose.Schema({
     },
     imagen:[
         {
-            public_id: {
-                type: String,
-                required: true
+            public_id:{
+                type:String,
+                required:true
             },
-            url: {
-                type: String,
-                required: true
+            url:{
+                type:String,
+                required:true
             }
         }
     ],
     categoria:{
-        type: String,
-        required: [true,"Por favor seleccione la categoria del producto."],
-        enum: {
-            values: [
-                "Color del producto",
-                "Tamaño del producto",
-                "Accesorios",
-                "Velocidad",
-                "Juegos de mesa",
-                "Tipo de llantas",
-                "Deportivos",
-                "Semiprofesionales",
-                "Profesionales",
-                "Acuatico",
-                "Terrestre",
-                "Aereo"
+        type:String,
+        required:[true,"Por favor seleccione la categoria del producto."],
+        enum:{
+            values:[
+                "bicicleta montaña",
+                "bicicorss",
+                "infantil",
+                "bicicleta pista",
             ]
         }
     },
-    vendededor:{
-        type: String,
-        required: [true,"Por favor registre el vendedor del producto adquirido."]
+    vendedor:{
+        type:String,
+        required:[true,"Por favor registre el vendedor de producto"]
     },
     inventario:{
         type: Number,
-        required: [true,"Por favor registre el stock del producto."],
-        maxLenght: [5,"Cantidad maxima del producto no puede sobrepasar 99999."],
-        default:0           
+        required:[true, "Por favor registre el stock del producto"],
+        maxLength:[5,"Cantidad maxima del producto no puede sobrepasar 99999"],
+        default:0
     },
     numCalificaciones:{
-        type: Number,
-        default: 0
+        type:Number,
+        default:0
     },
     opiniones:[
         {
             nombreCliente:{
-                type: String,
-                required: true
+                type:String,
+                required:true
             },
             rating:{
-                type: Number,
-                required: true
+                type:Number,
+                required:true
             },
             comentario:{
-                type: String,
-                required: true
+                type:String,
+                required:true
             }
         }
     ],
     fechaCreacion:{
-        type: Date,
-        default: Date.now
+        type:Date,
+        default:Date.now
     }
 
 })

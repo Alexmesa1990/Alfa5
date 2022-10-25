0d853b799c6f9a2013465ec67aa44f96ec19801b
alex mesa <alexmesa1990@hotmail.com>
2022-10-25 15:02:03 -0500

    se ejecuto los comondos que nos permiten agendar un producto, editarlo, eliminarlo

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la propuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") //Traemos la propuesta json desde el controlador
 
 router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct); //Establecemos la ruta
+router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+router.route('/producto/:id').put(updateProduct); //Creacion de la ruta de actialización por id
+router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
 
 module.exports=router;

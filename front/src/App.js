e25f735291c1bc65b406bc9b859c3e0b0e4d1b76
alex mesa <alexmesa1990@hotmail.com>
2022-10-26 15:46:59 -0500

    se intalo las herramientas de fron, tales como React y su interfas grafica

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tienda Especialista en Bicicletas Alfa5</h1>
+      <img src="./images/Alfa5.png" alt="Aqui va el logo"></img>
     </div>
   );
 }

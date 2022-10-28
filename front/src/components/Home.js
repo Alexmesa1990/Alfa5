79fc7f54be782a5ced5b2d3a81b2e5943eb6f32f
alex mesa <alexmesa1990@hotmail.com>
2022-10-28 00:39:42 -0500

    de manera dummies se genero interfas grafica agregando productos al front

---
+++ front/src/components/Home.js
@@ -0,0 +1,130 @@
+import React, { Fragment } from 'react'
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <h1 id="Encabezado_productos">Ultimos productos</h1>
+
+            <section id="productos" className='container mt-5'>
+                <div className='row'>
+
+                    {/*Producto 1*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Cross_BMX_amarilla.webp' alt="Cross_BMX_amarilla"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cross_BMX_amarilla</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">5 reviews</span>
+                                </div>
+                                <p className='card-text'>$900.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 2*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Cross_BMX_negra.jpg' alt="Cross_BMX_negra"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cross_BMX_negra</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">3 reviews</span>
+                                </div>
+                                <p className='card-text'>$1'200.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 3*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Montaña_Cliff_amarilla.jpg' alt="Montaña_Cliff_amarilla"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Montaña_Cliff_amarilla</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">12 reviews</span>
+                                </div>
+                                <p className='card-text'>$1'800.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 4*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Montaña_Cliff_gris.jpg' alt="Montaña_Cliff_gris"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Montaña_Cliff_gris</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">18 reviews</span>
+                                </div>
+                                <p className='card-text'>$1'800.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 5*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Montaña_Cliff_rosa.jpg' alt="Montaña_Cliff_rosa"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Montaña_Cliff_rosa</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">21 reviews</span>
+                                </div>
+                                <p className='card-text'>$1'600.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 6*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/Ruta_Cannondale_negra.webp' alt="Ruta_Cannondale_negra"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Ruta_Cannondale_negra</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id="No_de_opiniones">14 reviews</span>
+                                </div>
+                                <p className='card-text'>$3'600.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                    Ver detalle
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
+    )
+}
+
+export default Home
